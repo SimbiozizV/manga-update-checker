57012ae5832f08809e663e66f237019535618ef5
galcevskij <galchevskiy@farpost.com>
2022-12-08 00:04:28 +1000

    Добавлена сортировка в списке манги

--- src/components/MangaListTab/MangaListTab.tsx
+++ src/components/MangaListTab/MangaListTab.tsx
@@ -5,8 +5,17 @@ import { useAppSelector } from '../../hooks';
 import ExportButton from './ExportButton';
 import ImportButton from './ImportButton';
 import styled from '@emotion/styled';
+import { createSelector } from '@reduxjs/toolkit';
+import { Manga } from '../../types/Manga';
 
-const selector = (state: Store) => state.manga;
+const getManga = (state: Store) => state.manga;
+const selector = createSelector([getManga], (manga: Manga[]) => {
+    return [...manga].sort((a, b) => {
+        if (a.source > b.source) return -1;
+        if (a.source < b.source) return 1;
+        return 0;
+    });
+});
 
 const ButtonsWrap = styled.div`
     display: flex;
