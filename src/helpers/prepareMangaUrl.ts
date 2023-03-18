b2c820efb7537d105d1cedc50dad81071ea93719
galcevskij <galchevskiy@farpost.com>
2023-03-18 22:19:06 +1000

    v2.2.5

--- src/helpers/prepareMangaUrl.ts
+++ src/helpers/prepareMangaUrl.ts
@@ -4,16 +4,23 @@ import { SourceType } from '../enum';
 export default (url: string): string => {
     const type = determinateSourceType(url);
 
-    if (type && type === SourceType.MangaLib) {
-        const urlObj = new URL(url);
-        urlObj.searchParams.set('section', 'chapters');
-        return urlObj.toString();
+    const urlObj = new URL(url);
+    const splitPath = urlObj.pathname.replace(/^\//, '').split('/');
+
+    if (type === SourceType.AK || type === SourceType.ReadManga) {
+        return `${urlObj.origin}/${splitPath[0]}`;
     }
 
-    if (type && type === SourceType.Remanga) {
-        const urlObj = new URL(url);
-        urlObj.searchParams.set('p', 'content');
-        return urlObj.toString();
+    if (type === SourceType.MangaLib) {
+        return `${urlObj.origin}/${splitPath[0]}?section=chapters`;
+    }
+
+    if (type === SourceType.Desu) {
+        return `${urlObj.origin}/manga/${splitPath[1]}`;
+    }
+
+    if (type === SourceType.Remanga) {
+        return `${urlObj.origin}/manga/${splitPath[1]}?p=content`;
     }
 
     return url;
