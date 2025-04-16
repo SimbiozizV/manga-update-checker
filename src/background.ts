e6fa28a0e7ef7dbe74213a9028b9a88e010b2fb9
simbiozizv <simbioziz@gmail.com>
2025-04-16 14:36:17 +0700

    fix(Background): Обновление списка манги частями

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,14 @@
 import MangaStorage from './class/MangaStorage';
-import { STORAGE_KEY } from './constants';
+import { STORAGE_KEY, BATCH_SIZE } from './constants';
 import { updateManga } from './state/slices/mangaPage';
 import { getNewChaptersCount, setExtensionIconMode } from './helpers';
+import { Manga } from './types/Manga';
+
 chrome.runtime.onInstalled.addListener(async () => {
     const store = new MangaStorage(STORAGE_KEY);
     const { manga } = await store.getStorage();
 
-    setExtensionIconMode(getNewChaptersCount(manga));
+    await setExtensionIconMode(getNewChaptersCount(manga));
 
     chrome.alarms.get('periodic', a => {
         if (!a) chrome.alarms.create('periodic', { periodInMinutes: 10.0 });
@@ -17,23 +19,25 @@ chrome.alarms.onAlarm.addListener(async () => {
     const store = new MangaStorage(STORAGE_KEY);
     const { manga } = await store.getStorage();
 
-    const updatedManga = await Promise.allSettled(manga.map(updateManga));
-    const result = updatedManga.map((item, key) => {
-        return item.status === 'fulfilled'
-            ? {
-                  ...manga[key],
-                  image: item.value.image,
-                  title: item.value.title,
-                  url: item.value.url,
-                  lastChapter: item.value.lastChapter,
-                  status: item.value.status,
-              }
-            : manga[key];
-    });
+    let index = 0;
+    let result: Manga[] = [];
+    while (index < manga.length) {
+        const batch = manga.slice(index, index + BATCH_SIZE);
+        const updatedManga = await Promise.all(batch.map(updateManga));
+        const mangaBatch = updatedManga.map((item, key) => {
+            return {
+                ...batch[key],
+                image: item.image,
+                title: item.title,
+                mirrors: item.mirrors,
+            };
+        });
+        result = [...result, ...mangaBatch];
+        index += BATCH_SIZE;
+    }
 
     await store.setMangaList(result);
-
-    setExtensionIconMode(getNewChaptersCount(result));
+    await setExtensionIconMode(getNewChaptersCount(result));
 });
 
 chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
